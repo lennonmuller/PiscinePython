ae574f84286b03ab3537d9a2d3436ee7dee9b48b
lennonmuller <lennon-muller@hotmail.com>
2026-02-21 12:22:57 +0000

    py00

---
+++ Py00/ex3/ft_plant_age.py
@@ -0,0 +1,6 @@
+def	ft_plant_age():
+	age_plant = int(input("Enter plant age in days: "))
+	if age_plant > 60:
+		print("Plant is ready to harvest!")
+	else:
+		print("Plant needs more time to grow.")
