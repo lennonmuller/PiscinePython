96b7d99b6db924007fec8a56c51c9872c24f1111
lennonmuller <lennon-muller@hotmail.com>
2026-03-22 14:31:02 +0000

    Implementation of import_transmutation and defining potions functions

---
+++ Py06/alchemy/potions.py
@@ -0,0 +1,22 @@
+import alchemy.elements
+
+
+def healing_potion() -> str:
+    return f"Healing potion brewed with {alchemy.elements.create_fire()} \
+and {alchemy.elements.create_water()}"
+
+
+def strength_potion() -> str:
+    return f"Strength potion brewed with {alchemy.elements.create_earth()} \
+and {alchemy.elements.create_fire()}"
+
+
+def invisibility_potion() -> str:
+    return f"Invisibility potion brewed with {alchemy.elements.create_air()} \
+and {alchemy.elements.create_water()}"
+
+
+def wisdom_potion() -> str:
+    return f"Wisdom potion brewed with all elements: \
+{alchemy.elements.create_air()}, {alchemy.elements.create_water()}, \
+{alchemy.elements.create_earth()} and {alchemy.elements.create_fire()}"
