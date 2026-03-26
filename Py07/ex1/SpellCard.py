48b1393f0bd20ff61ba75d599cd5e4220b3c34e0
lennonmuller <lennon-muller@hotmail.com>
2026-03-26 10:26:38 +0000

    Concrete implementation of SpellCard

---
+++ Py07/ex1/SpellCard.py
@@ -0,0 +1,16 @@
+from ex0.Card import Card
+
+
+class SpellCard(Card):
+    def __init__(self, name: str, cost: int, rarity: str,
+                 effect_type: str) -> None:
+        super().__init__(name, cost, rarity)
+        self.effect_type = effect_type
+
+    def play(self, game_state: dict) -> dict:
+        game_state = {"card_played": self.name, "mana_used": self.cost,
+                      "effect": self.effect_type}
+        return game_state
+
+    def resolve_effect(self, targets: list) -> dict:
+        pass
