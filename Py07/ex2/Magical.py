95a9fe1d356a45b410c8cf55bc3946c0a08dbcb9
lennonmuller <lennon-muller@hotmail.com>
2026-03-27 10:26:04 +0000

    Implementation of Magical abstract method

---
+++ Py07/ex2/Magical.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class Magical(ABC):
+    def __init__(self, mana_cost: int, spell_power: int) -> None:
+        self.mana_cost = mana_cost
+        self.spell_power = spell_power
+
+    @abstractmethod
+    def cast_spell(self, spell_name: str, targets: list) -> dict:
+        pass
+
+    @abstractmethod
+    def channel_mana(self, amount: int) -> dict:
+        pass
+
+    @abstractmethod
+    def get_magic_stats(self) -> dict:
+        pass
